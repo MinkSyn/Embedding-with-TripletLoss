4fbc69c276d589f9147986985741c980d2614801
minhdc <dangcongminh0203@gmail.com>
2023-04-07 15:56:45 +0700

    Pre-commit size input

--- train.py
+++ train.py
@@ -129,9 +129,6 @@ class Trainer:
                 tqdm(train_dl, desc=f"Training epoch {epoch:>2d}")
             ):
                 input, target = batch
-                print(input.shape)
-                print(target.shape)
-                exit()
                 input = input.to(self.device)
 
                 output = self.model(input)
