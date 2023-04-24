c28da1fde30a3cc77a199ffa9be2cb3c2ed8d60e
minksyn <dangcongminh0203@gmail.com>
2023-04-24 10:22:19 +0700

    Fix dataset for card-test

--- dataset.py
+++ dataset.py
@@ -19,20 +19,33 @@ class PatchCoreDataset(Dataset):
 
         self.samples = self.get_samples(root)
 
+    # def get_samples(self, root):
+    #     samples = []
+    #     for card_type in os.listdir(root):
+    #         id_class = CardID[card_type].value
+    #         card_path = os.path.join(root, card_type)
+
+    #         for quality_type in os.listdir(card_path):
+    #             quality_path = os.path.join(card_path, quality_type)
+
+    #             for file in os.listdir(quality_path):
+    #                 filename = os.fsdecode(file)
+    #                 if is_image_file(filename):
+    #                     path = os.path.join(quality_path, filename)
+    #                     samples.append((path, id_class))
+    #     return samples
+    
     def get_samples(self, root):
         samples = []
         for card_type in os.listdir(root):
             id_class = CardID[card_type].value
             card_path = os.path.join(root, card_type)
 
-            for quality_type in os.listdir(card_path):
-                quality_path = os.path.join(card_path, quality_type)
-
-                for file in os.listdir(quality_path):
-                    filename = os.fsdecode(file)
-                    if is_image_file(filename):
-                        path = os.path.join(quality_path, filename)
-                        samples.append((path, id_class))
+            for file in os.listdir(card_path):
+                filename = os.fsdecode(file)
+                if is_image_file(filename):
+                    path = os.path.join(card_path, filename)
+                    samples.append((path, id_class))
         return samples
 
     def __len__(self):
