6ee269d5ed6c2ef36930530499b4da5a12222628
thiendang <thien.dang@ncc.asia>
2023-06-19 20:32:12 +0700

    fix

--- evaluate.py
+++ evaluate.py
@@ -1,188 +1,337 @@
-import os
-
-import numpy as np
-import pandas as pd
 import torch
-from loguru import logger
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC, LinearSVC
-from torch.utils.data import DataLoader
-from torch.utils.data.sampler import SubsetRandomSampler
-
-from dataset import ArcfaceDataset
-from model import resnet_face18
-from tool import get_tfms
+import torch.nn as nn
+import math
+import torch.utils.model_zoo as model_zoo
+import torch.nn.utils.weight_norm as weight_norm
+import torch.nn.functional as F
 
 
-class ArcfaceEvaluate:
-    def __init__(
-        self,
-        root,
-        out_root,
-        epoch,
-        weight_path,
-        device,
-        model=None,
-        img_size=(128, 128),
-        batch_size=16,
-        norm_stats='imagenet',
-    ):
-        self.device = device
-        self.root = root
-        self.out_root = out_root
-        self.epoch = epoch
-        os.makedirs(self.out_root, exist_ok=True)
+# __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+#            'resnet152']
 
-        logger.info('Evaluate:')
-        if model is None:
-            self.model = self.load_model(weight_path)
-        else:
-            self.model = model
 
-        transform = get_tfms(img_size=img_size, norm_stats=norm_stats)
-        self.train_loader, self.val_loader = self.get_loader(
-            batch_size=batch_size, transform=transform
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class IRBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, use_se=True):
+        super(IRBlock, self).__init__()
+        self.bn0 = nn.BatchNorm2d(inplanes)
+        self.conv1 = conv3x3(inplanes, inplanes)
+        self.bn1 = nn.BatchNorm2d(inplanes)
+        self.prelu = nn.PReLU()
+        self.conv2 = conv3x3(inplanes, planes, stride)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.use_se = use_se
+        if self.use_se:
+            self.se = SEBlock(planes)
+
+    def forward(self, x):
+        residual = x
+        out = self.bn0(x)
+        out = self.conv1(out)
+        out = self.bn1(out)
+        out = self.prelu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.use_se:
+            out = self.se(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.prelu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class SEBlock(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                nn.PReLU(),
+                nn.Linear(channel // reduction, channel),
+                nn.Sigmoid()
         )
 
-        self.KNN_classify = KNeighborsClassifier()
-        self.SVM_classify = SVC()
-        self.LinearSVM_classify = LinearSVC()
-        self.RF_classify = RandomForestClassifier()
-
-    def load_model(self, weight_path):
-        if not os.path.exists(weight_path):
-            raise Exception(f'Not exits path: {weight_path}')
-        print(f"Loading Model checkpoint {weight_path} ...")
-        model_state = torch.load(weight_path, map_location=self.device)
-        
-        model = resnet_face18(use_se=True)
-        try:
-            model.load_state_dict(model_state)
-        except: 
-            from collections import OrderedDict
-            new_state_dict = OrderedDict()
-            for k, v in model_state.items():
-                name = k.replace('module.', '', 1)
-                # name = k.replace('model.', '', 1)
-                new_state_dict[name] = v
-            model.load_state_dict(new_state_dict)
-            
-        model = model.to(self.device)
-        model.eval()
-        return model
-
-    def get_loader(self, batch_size, transform):
-        dataset = ArcfaceDataset(split='test', root=self.root, transforms=transform)
-        dataset_size = len(dataset)
-        indices = list(range(dataset_size))
-        split = int(np.floor(0.2 * dataset_size))
-
-        np.random.seed(1235)
-        np.random.shuffle(indices)
-        train_indices, val_indices = indices[:split], indices[split:]
-
-        train_sampler = SubsetRandomSampler(train_indices)
-        valid_sampler = SubsetRandomSampler(val_indices)
-
-        train_loader = DataLoader(dataset, batch_size=batch_size, sampler=train_sampler)
-        val_loader = DataLoader(dataset, batch_size=batch_size, sampler=valid_sampler)
-        return train_loader, val_loader
-
-    def embedding_dataset(self):
-        self.model.eval()
-        torch.cuda.empty_cache()
-        for idx, (input, target) in enumerate(self.train_loader):
-            input = input.to(self.device)
-            output = self.model(input)
-            output = output.detach().cpu()
-
-            if idx == 0:
-                embedding_train = output
-                target_train = target
-            else:
-                embedding_train = torch.cat(
-                    (embedding_train, output),
-                    dim=0,
-                )
-                target_train = torch.cat(
-                    (target_train, target),
-                    dim=0,
-                )
-
-        for idx, (input, target) in enumerate(self.val_loader):
-            input = input.to(self.device)
-            output = self.model(input)
-            output = output.detach().cpu()
-
-            if idx == 0:
-                embedding_val = output
-                target_val = target
-            else:
-                embedding_val = torch.cat(
-                    (embedding_val, output),
-                    dim=0,
-                )
-                target_val = torch.cat(
-                    (target_val, target),
-                    dim=0,
-                )
-
-        info_embedding = {
-            'embedding_train': embedding_train.numpy(),
-            'target_train': target_train.numpy(),
-            'embedding_val': embedding_val.numpy(),
-            'target_val': target_val.numpy(),
-        }
-        torch.save(info_embedding, self.out_root + f'/res_embedding_{self.epoch}.pt')
-
-    def clasification(self):
-        info_embedding = torch.load(self.out_root + f'/res_embedding_{self.epoch}.pt')
-
-        embedding_train = info_embedding['embedding_train']
-        target_train = info_embedding['target_train']
-
-        embedding_val = info_embedding['embedding_val']
-        target_val = info_embedding['target_val']
-
-        size_dict = {
-            'train datasets': embedding_train.shape,
-            'train labels': target_train.shape,
-            'valid datasets': embedding_val.shape,
-            'valid labels': target_val.shape,
-        }
-        
-        print('Size of embedding:')
-        print(pd.DataFrame({"size": size_dict.values()}, index=size_dict.keys()))
-        print(("-" * 80))
-        
-        print('KNN classification:')
-        self.KNN_classify.fit(embedding_train, target_train)
-        KNN_pred = self.KNN_classify.score(embedding_val, target_val)
-        print(f'Accuracy KNN: {KNN_pred}')
-        print(("-" * 80))
-
-        print('SVM classification:')
-        self.SVM_classify.fit(embedding_train, target_train)
-        SVM_pred = self.SVM_classify.score(embedding_val, target_val)
-        print(f'Accuracy SVM: {SVM_pred}')
-        print(("-" * 80))
-
-        # print('Linear SVM classification:')
-        # self.LinearSVM_classify.fit(embedding_train, target_train)
-        # LSVM_pred = self.LinearSVM_classify.score(embedding_val, target_val)
-        # print(f'Accuracy Linear SVM: {LSVM_pred}')
-        # print(("-" * 80))
-
-        print('Random forest classification:')
-        self.RF_classify.fit(embedding_train, target_train)
-        RF_pred = self.RF_classify.score(embedding_val, target_val)
-        print(f'Accuracy Random Forest: {RF_pred}')
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
 
 
-def main():
-    pass
+class ResNetFace(nn.Module):
+    def __init__(self, block, layers, use_se=True):
+        self.inplanes = 64
+        self.use_se = use_se
+        super(ResNetFace, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.prelu = nn.PReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.bn4 = nn.BatchNorm2d(512)
+        self.dropout = nn.Dropout()
+        self.fc5 = nn.Linear(512 * 8 * 8, 512)
+        self.bn5 = nn.BatchNorm1d(512)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, use_se=self.use_se))
+        self.inplanes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, use_se=self.use_se))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.prelu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.bn4(x)
+        x = self.dropout(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc5(x)
+        x = self.bn5(x)
+
+        return x
 
 
-if __name__ == '__main__':
-    main()
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+        #                        bias=False)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=2)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        # self.avgpool = nn.AvgPool2d(8, stride=1)
+        # self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc5 = nn.Linear(512 * 8 * 8, 512)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        # x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        # x = nn.AvgPool2d(kernel_size=x.size()[2:])(x)
+        # x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc5(x)
+
+        return x
+
+
+def resnet18(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    return model
+
+
+def resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
+    return model
+
+
+def resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+    return model
+
+
+def resnet101(pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
+    return model
+
+
+def resnet152(pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
+    return model
+
+
+def resnet_face18(use_se=True, **kwargs):
+    model = ResNetFace(IRBlock, [2, 2, 2, 2], use_se=use_se, **kwargs)
+    return model
